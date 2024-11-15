f5def62380486223948fd9fecebe68abe75395c1
Samuel777999 <samuel.dominguezlopez@cesunbc.edu.mx>
2024-11-14 20:23:41 -0800

    Class Activity 1

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Navbar from './components/navbar.jsx';
+import Footer from './components/footer.jsx';
+
+function App() {
+  return (
+    <div className='App'>
+      <Navbar></Navbar>
+      <div className='content'>
+        <h1>Hello World React!</h1>
+      </div>
+        <Footer></Footer>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/footer.jsx
@@ -0,0 +1,18 @@
+import "./styles/footer.css"
+
+function Footer(){
+    return(
+        <div>
+            <nav>
+                <div className="navbar-footer">
+                    <a href="">Home</a>
+                    <a href="">About</a>
+                    <a href="">Contact</a>
+                    <a href="">Created by: Samuel Dominguez</a>
+                </div>
+            </nav>
+        </div>
+    );
+}
+
+export default Footer;
